a5159dd8f9362b1656ab0a4a6d9ede726932eb57
mello <mellomainmail@gmail.com>
2022-09-16 23:15:23 +0200

    - Added /Items - Pre-created settings.yml - Edited workspace.xml - Added Factors.java entity constructor

--- src/main/java/pluginprovider/objects/Factors.java
+++ src/main/java/pluginprovider/objects/Factors.java
@@ -3,7 +3,9 @@ package pluginprovider.objects;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
 import pluginprovider.enums.factors.FactorsDataType;
 import pluginprovider.enums.factors.valuetypes.FactorsLookingValueType;
 import pluginprovider.enums.factors.valuetypes.FactorsTimeValueType;
@@ -49,6 +51,53 @@ public class Factors {
         FactorsLookingValueType up_down = FactorsLookingValueType.STRAIGHT;
         if (e.getPlayer().getLocation().getPitch() < -30) up_down = FactorsLookingValueType.UP;
         if (e.getPlayer().getLocation().getPitch() > 30) up_down = FactorsLookingValueType.DOWN;
+        //
+        // Register
+        //
+        dataType_dataValue.put(FactorsDataType.TIME, day_night);
+        dataType_dataValue.put(FactorsDataType.PERMISSION, permission);
+        dataType_dataValue.put(FactorsDataType.WORLD, worldName);
+        dataType_dataValue.put(FactorsDataType.BLOCK_ENTITY, blockType);
+        dataType_dataValue.put(FactorsDataType.SNEAKING, isSneaking);
+        dataType_dataValue.put(FactorsDataType.BIOME, biomeName);
+        dataType_dataValue.put(FactorsDataType.RAINING, isRaining);
+        dataType_dataValue.put(FactorsDataType.KILLED_DESTROYED_BY, playerName);
+        dataType_dataValue.put(FactorsDataType.FORTUNE_LOOTING, hasFortune);
+        dataType_dataValue.put(FactorsDataType.LOOKING, up_down);
+    }
+    public Factors(final EntityDeathEvent e, Player killer, String... permissions) {
+        isBlock = true;
+        dataType_dataValue = new HashMap<>();
+        // Time
+        FactorsTimeValueType day_night;
+        if (e.getEntity().getWorld().getTime() < 12300 | e.getEntity().getWorld().getTime() > 23850) day_night = FactorsTimeValueType.DAY;
+        else day_night = FactorsTimeValueType.NIGHT;
+        // Permission
+        boolean permission = true;
+        for (String var : permissions) {
+            if (!e.getEntity().hasPermission(var)) permission = false;
+        }
+        // World
+        String worldName = e.getEntity().getWorld().getName();
+        // -> Block / !-> Entity
+        EntityType blockType = e.getEntity().getType();
+        // Sneaking
+        boolean isSneaking = killer.isSneaking();
+        // Biome name
+        String biomeName = e.getEntity().getLocation().getBlock().getBiome().name();
+        // Raining
+        boolean isRaining = e.getEntity().getWorld().hasStorm();
+        // Killed / Destroyed by
+        String playerName = killer.getName();
+        // !-> Looting / -> Fortune
+        boolean canCheck = killer.getItemInUse() != null;
+        boolean hasFortune;
+        if (canCheck) hasFortune = killer.getItemInUse().getEnchantments().containsKey(Enchantment.LOOT_BONUS_BLOCKS);
+        else hasFortune = false;
+        // Looking
+        FactorsLookingValueType up_down = FactorsLookingValueType.STRAIGHT;
+        if (killer.getLocation().getPitch() < -30) up_down = FactorsLookingValueType.UP;
+        if (killer.getLocation().getPitch() > 30) up_down = FactorsLookingValueType.DOWN;
         //
         // Register
         //
